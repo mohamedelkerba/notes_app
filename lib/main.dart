d69f04d79f77e5f13824e76ada76eef825b48ac7
Mohamed Elkerba <mohamedelkerba0@gmial.com>
2024-10-14 14:10:25 +0300

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:training/add_notes_cubit/add_note_cubit.dart';
 import 'package:training/models/note_model.dart';
 import 'views/notes_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -15,13 +17,18 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.transparent,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=> AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: Colors.transparent,
+        ),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
   }
 }
